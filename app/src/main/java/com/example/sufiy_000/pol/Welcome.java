bfc4365425d1b01717155695481af8233f20a972
notSufi <sufi@sufigaffar.com>
2015-07-27 14:50:44 +0100

    Test

--- app/src/main/java/com/example/sufiy_000/pol/Welcome.java
+++ app/src/main/java/com/example/sufiy_000/pol/Welcome.java
@@ -12,6 +12,8 @@ public class Welcome extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        int hello;
     }
 
     @Override
