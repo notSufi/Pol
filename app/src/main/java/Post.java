5fb32af7567e8a0e89abbc73be22b26212625274
Thomas Peregrine <tomfperegrine@googlemail.com>
2015-07-28 08:56:29 +0100

    Added Getters and Setters for the Post class

--- app/src/main/java/Post.java
+++ app/src/main/java/Post.java
@@ -4,11 +4,11 @@ import java.util.Date;
  * Created by Thomas on 27/07/2015.
  */
 public class Post {
-    public int m_parentId;
-    public String m_posterId;
-    public Date m_timestamp;
-    public int m_up_votes;
-    public int m_flagged_amount;
+    private int m_parentId;
+    private String m_posterId;
+    private Date m_timestamp;
+    private int m_up_votes;
+    private int m_flagged_amount;
 
     public Post(int m_parentId, String m_posterId, Date m_timestamp, int m_up_votes, int m_flagged_amount) {
         this.m_parentId = m_parentId;
@@ -20,5 +20,45 @@ public class Post {
 
     public boolean IsThread() {
         return m_parentId == 0;
+    }
+
+    public int getM_parentId() {
+        return m_parentId;
+    }
+
+    public void setM_parentId(int m_parentId) {
+        this.m_parentId = m_parentId;
+    }
+
+    public String getM_posterId() {
+        return m_posterId;
+    }
+
+    public void setM_posterId(String m_posterId) {
+        this.m_posterId = m_posterId;
+    }
+
+    public Date getM_timestamp() {
+        return m_timestamp;
+    }
+
+    public void setM_timestamp(Date m_timestamp) {
+        this.m_timestamp = m_timestamp;
+    }
+
+    public int getM_up_votes() {
+        return m_up_votes;
+    }
+
+    public void setM_up_votes(int m_up_votes) {
+        this.m_up_votes = m_up_votes;
+    }
+
+    public int getM_flagged_amount() {
+        return m_flagged_amount;
+    }
+
+    public void setM_flagged_amount(int m_flagged_amount) {
+        this.m_flagged_amount = m_flagged_amount;
     }
 }
