581d5f061944835a624b2de64fcc6756e6f89686
Thomas Peregrine <tomfperegrine@googlemail.com>
2015-07-28 14:18:27 +0100

    Worked on welcome activity

--- app/src/main/java/com/example/sufiy_000/pol/Welcome.java
+++ app/src/main/java/com/example/sufiy_000/pol/Welcome.java
@@ -1,20 +1,26 @@
 package com.example.sufiy_000.pol;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
-import java.util.Locale;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 
 public class Welcome extends ActionBarActivity {
 
-    private Locale m_locale;
-    private String m_localeString;
+    private double m_lat;
+    private double m_long;
+    private String text;
 
     private Handler m_Handler = new Handler();
+    private boolean CanSwitch = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +29,24 @@ public class Welcome extends ActionBarActivity {
 
         // Check if user exists
 
-        m_locale = getResources().getConfiguration().locale;
-        m_localeString = m_locale.getDisplayCountry();
+        TextView testView = (TextView) findViewById(R.id.textView2);
+
+       /* Location m_deviceLocation = new Location("");
+        double m_lat = m_deviceLocation.getLatitude();
+        double m_long = m_deviceLocation.getLongitude();*/
+
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(location == null){
+            text = "Location not found";
+        }else {
+            m_long = location.getLongitude();
+            m_lat = location.getLatitude();
+            text = String.valueOf(m_lat)+","+String.valueOf(m_long);
+            CanSwitch = true;
+        }
+
+        testView.setText(text);
 
         m_Handler.postDelayed(new Runnable() {
             @Override
@@ -35,28 +57,12 @@ public class Welcome extends ActionBarActivity {
     }
 
     private void doStuff () {
-        Toast.makeText(this, "Switch activity now", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_welcome, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (CanSwitch) {
+            Intent intent = new Intent(this, Home.class);
+            startActivity(intent);
         }
-
-        return super.onOptionsItemSelected(item);
+        else {
+            Toast.makeText(this, "Switch activity now", Toast.LENGTH_SHORT).show();
+        }
     }
 }
