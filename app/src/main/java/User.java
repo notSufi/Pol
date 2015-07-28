79f01617016d397c2eb5d120c172df2f7931cacb
Thomas Peregrine <tomfperegrine@googlemail.com>
2015-07-28 10:46:07 +0100

    Refactored User

--- app/src/main/java/User.java
+++ app/src/main/java/User.java
@@ -6,39 +6,38 @@ import java.util.Date;
 public class User {
 
     private String m_androidId;
-    private Date m_signupTime;
+    private Date m_signUpTime;
     private int m_admin;
-
-    public String getM_androidId() {
-        return m_androidId;
-    }
-
-    public void setM_androidId(String m_androidId) {
-        this.m_androidId = m_androidId;
-    }
-
-    public Date getM_signupTime() {
-        return m_signupTime;
-    }
-
-    public void setM_signupTime(Date m_signupTime) {
-        this.m_signupTime = m_signupTime;
-    }
-
-    public int getM_admin() {
-        return m_admin;
-    }
-
-    public void setM_admin(int m_admin) {
-        this.m_admin = m_admin;
-    }
 
     public User(String m_androidId, Date m_signupTime, int m_admin){
 
         this.m_androidId = m_androidId;
-        this.m_signupTime = m_signupTime;
+        this.m_signUpTime = m_signupTime;
         this.m_admin = m_admin;
 
     }
 
+    public String getAndroidId() {
+        return m_androidId;
+    }
+
+    public void setAndroidId(String m_androidId) {
+        this.m_androidId = m_androidId;
+    }
+
+    public Date getSignUpTime() {
+        return m_signUpTime;
+    }
+
+    public void setSignUpTime(Date m_signUpTime) {
+        this.m_signUpTime = m_signUpTime;
+    }
+
+    public int getAdmin() {
+        return m_admin;
+    }
+
+    public void setAdmin(int m_admin) {
+        this.m_admin = m_admin;
+    }
 }
