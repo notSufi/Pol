a139351def81e2cab23956f8e59c344bc8379336
Thomas Peregrine <tomfperegrine@googlemail.com>
2015-07-28 09:32:10 +0100

    Added Thread parent constant

--- app/src/main/java/Post.java
+++ app/src/main/java/Post.java
@@ -4,61 +4,64 @@ import java.util.Date;
  * Created by Thomas on 27/07/2015.
  */
 public class Post {
+
+    final public int THREAD_PARENT_INDEX = 0;
+
     private int m_parentId;
     private String m_posterId;
     private Date m_timestamp;
     private int m_up_votes;
-    private int m_flagged_amount;
+    private int m_flaggedAmount;
 
     public Post(int m_parentId, String m_posterId, Date m_timestamp, int m_up_votes, int m_flagged_amount) {
         this.m_parentId = m_parentId;
         this.m_posterId = m_posterId;
         this.m_timestamp = m_timestamp;
         this.m_up_votes = m_up_votes;
-        this.m_flagged_amount = m_flagged_amount;
+        this.m_flaggedAmount = m_flagged_amount;
     }
 
     public boolean IsThread() {
-        return m_parentId == 0;
+        return m_parentId == THREAD_PARENT_INDEX;
     }
 
-    public int getM_parentId() {
+    public int getParentId() {
         return m_parentId;
     }
 
-    public void setM_parentId(int m_parentId) {
+    public void setParentId(int m_parentId) {
         this.m_parentId = m_parentId;
     }
 
-    public String getM_posterId() {
+    public String getPosterId() {
         return m_posterId;
     }
 
-    public void setM_posterId(String m_posterId) {
+    public void setPosterId(String m_posterId) {
         this.m_posterId = m_posterId;
     }
 
-    public Date getM_timestamp() {
+    public Date getTimestamp() {
         return m_timestamp;
     }
 
-    public void setM_timestamp(Date m_timestamp) {
+    public void setTimestamp(Date m_timestamp) {
         this.m_timestamp = m_timestamp;
     }
 
-    public int getM_up_votes() {
+    public int getUpVotes() {
         return m_up_votes;
     }
 
-    public void setM_up_votes(int m_up_votes) {
+    public void setUpVotes(int m_up_votes) {
         this.m_up_votes = m_up_votes;
     }
 
-    public int getM_flagged_amount() {
-        return m_flagged_amount;
+    public int getFlaggedAmount() {
+        return m_flaggedAmount;
     }
 
-    public void setM_flagged_amount(int m_flagged_amount) {
-        this.m_flagged_amount = m_flagged_amount;
+    public void setFlaggedAmount(int m_flaggedAmount) {
+        this.m_flaggedAmount = m_flaggedAmount;
     }
 }
