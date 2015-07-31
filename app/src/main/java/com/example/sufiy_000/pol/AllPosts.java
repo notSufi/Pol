8a6f47e2aa412781679d9609b36a19f1eeb4e916
Thomas Peregrine <tomfperegrine@googlemail.com>
2015-07-31 16:08:35 +0100

    Added list for threads

--- app/src/main/java/com/example/sufiy_000/pol/AllPosts.java
+++ app/src/main/java/com/example/sufiy_000/pol/AllPosts.java
@@ -4,20 +4,28 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.example.sufiy_000.pol.classes.AsyncHttpGet;
+import com.example.sufiy_000.pol.classes.Candidate;
+import com.example.sufiy_000.pol.classes.User;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AllPosts.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AllPosts#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class AllPosts extends android.support.v4.app.Fragment {
+
+    ListView m_listView;
+    private ArrayAdapter<String> m_adapter;
+    private ArrayList<String> m_arrayList = new ArrayList<String>();
+
+    Home parentActivity;
+
+    public User currentUser;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,6 +33,29 @@ public class AllPosts extends android.support.v4.app.Fragment {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_all_posts, container, false);
 
+        parentActivity = (Home) getActivity();
+
+        m_listView = (ListView) rootView.findViewById(R.id.ThreadsList);
+        m_adapter = new ArrayAdapter<String>(parentActivity.getApplicationContext(),
+                R.layout.thread_list_item, m_arrayList);
+
+        m_listView.setAdapter(m_adapter);
+
+        m_arrayList.add("UKIP Policies");
+        m_arrayList.add("Tory Policies");
+        m_adapter.notifyDataSetChanged();
+
+        GetThreads test = new GetThreads();
+        test.execute("http://sufigaffar.com/pol/?query=post&action=return&limit=10&order=timestamp&parent=0&constituency=wealden");
+
         return rootView;
+    }
+
+    class GetThreads extends AsyncHttpGet {
+        @Override
+        protected void onPostExecute(String response) {
+            super.onPostExecute(response);
+            Log.d("Threads Response", response);
+        }
     }
 }
